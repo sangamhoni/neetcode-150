32069d5bdac22e2c0bb0b2339d9ebed24bbfaad9
sangamhoni <87515320+subsangam@users.noreply.github.com>
2024-10-20 11:34:59 -0500

    Solved Two Sum II in O(1) time

--- 02-two-pointers/02-two-sum-II/README.md
+++ 02-two-pointers/02-two-sum-II/README.md
@@ -1,11 +1,12 @@
 ## Asymptotic Analysis  
 Time Complexity: `O(n)`  
-Space Complexity: `O(n)`  
+Space Complexity: `O(1)`  
 
 &nbsp;  
 
 ## Personal Stats
 | Date Solved | Time Taken |
 | ----------- | ---------- |
+| 10-20-2024 | 3.5min |  
 | 10-16-2024 | 2min |  
 | 10-15-2024 | 4min |  
--- 02-two-pointers/02-two-sum-II/solution-1.py
+++ 02-two-pointers/02-two-sum-II/solution-1.py
@@ -5,6 +5,30 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+
+        l = 0
+        r = len(numbers)-1
+        while l<r:
+            sum = numbers[l] + numbers[r]
+
+            print(sum)
+
+            if sum > target:
+                r -= 1
+            elif sum < target:
+                l += 1
+            else:
+                return [l+1, r+1]
+
+
+#  SOLUTION WITH O(N) TIME COMPLEXITY
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         hashMap = dict()
 
         for i, num in enumerate(numbers):
